4a3be351e921d57443950dd42bd7216dc3533a19
Ian Parker <IanPark@hpsvikings.org>
2019-02-26 14:12:08 -0500

    Project 4 complete

--- Project4/main.go
+++ Project4/main.go
@@ -1,24 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
-func reverseString(numString string) (string, string) {
+func reverseString(numString string) string {
 
 	chars := []rune(numString)
 	rChars := make([]rune, len(numString))
-	for _, v := range chars {
-		fmt.Printf("%v\t%c\n", v, v)
-	}
 
-	for i := len(chars) - 1; i > 0; i-- {
+	for i := len(chars) - 1; i >= 0; i-- {
 		rChars[i] = chars[(len(chars)-1)-i]
 	}
-	return string(chars), string(rChars)
+	return string(rChars)
+}
+
+func isPalindrome(numString string) bool {
+	if numString == reverseString(numString) {
+		return true
+	}
+	return false
 }
 
 func main() {
 
-	x, y := reverseString("1234")
-	fmt.Println(x)
-	fmt.Println(y)
+	maxDigits := 3
+
+	highNumSlice := make([]rune, maxDigits)
+	for i := 0; i < maxDigits; i++ {
+		highNumSlice[i] = '9'
+	}
+	highNumString := string(highNumSlice)
+	highNum, _ := strconv.Atoi(highNumString)
+
+	var product int
+	var maxPal int
+	for i := highNum; i > 0; i-- {
+		for j := highNum; j > 0; j-- {
+			product = i * j
+			if isPalindrome(strconv.Itoa(product)) {
+				if product > maxPal {
+					maxPal = product
+				}
+			}
+		}
+	}
+	fmt.Println(maxPal)
+
 }
